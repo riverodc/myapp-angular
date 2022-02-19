f2d2b750663519837fa8a6381d9a6f5f33480c9c
Daniel <riveroweb.daniel@gmail.com>
2022-02-19 14:23:05 -0500

    Proyect for Learn Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Hi first class';
+  name = 'Daniel';
+  a = 1;
+  b = 'Luke'; 
+
+  
+  bibleBooks = ['Exodus', 'Genesis', 'Revelations', 'Jhon']; 
+  bibleBookName = '';
+/* Nuevo metodo para agregar un elemento a mi lista */
+  addBibleBook() {
+    this.bibleBooks.push(this.bibleBookName);
+    this.bibleBookName = '';
+  }
+
+/* Mi metodo para cambiar titulo*/
+  changeTitle() {
+    /* Para hacer refencia a variables utilizo la palabra 
+    clave this que hace una llamada a mis variables*/
+    this.title = 'New Title method > ok'; 
+    /* Llamo el metodo privado desde otro metodo */
+  }
+
+  empty() {
+    this.bibleBooks =[]; 
+  }
+
+
+}
