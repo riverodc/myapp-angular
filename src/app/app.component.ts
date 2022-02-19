b06a23159f1931ae57d36fc22fd87f58498c2780
Daniel <riveroweb.daniel@gmail.com>
2022-02-19 15:41:56 -0500

    Estructuras de Control ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hi first class';
   name = 'Daniel';
-  a = 1;
-  b = 'Luke'; 
+  
 
   
   bibleBooks = ['Exodus', 'Genesis', 'Revelations', 'Jhon']; 
